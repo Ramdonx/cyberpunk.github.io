cf683095b6af960fc03feec30e413115587a6df8
Ramdonx <40654074+Ramdonx@users.noreply.github.com>
2020-09-12 23:50:44 -0500

    Add files via upload

--- js/script - Copy.js
+++ js/script - Copy.js
@@ -4,7 +4,6 @@ const $  = (e) => document.querySelector(e),
 function engines () {
   return {
     g: ['https://google.com/search?q=', 'Google'],
-    n: ['https://nhentai.net/search/?q=', 'Nhentai'],
     i: ['https://ixquick.com/do/search?q=', 'Ixquick'],
     d: ['https://duckduckgo.com/html?q=', 'DuckDuckGo'],
     y: ['https://youtube.com/results?search_query=', 'Youtube'],
